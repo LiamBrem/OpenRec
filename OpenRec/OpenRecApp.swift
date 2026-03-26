bf364837494b71668db0d9d0f5a2176a8bf3db68
liambrem <liam.e.brem@gmail.com>
2026-03-26 18:12:17 -0400

    Initial Commit

---
+++ OpenRec/OpenRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRecApp.swift
+//  OpenRec
+//
+//  Created by Liam Brem on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
